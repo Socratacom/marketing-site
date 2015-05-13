120810738e200704ee9a59307c1e174cfbd1ecd5
Chris Griffing <chris.griffing@freshconsulting.com>
2015-05-13 14:58:58 -0700

    added code to functions.php that adds stub ACF functions when ACF is disabled

--- wp-content/themes/sage/functions.php
+++ wp-content/themes/sage/functions.php
@@ -31,3 +31,25 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+
+
+//Bootstrapping ACF functions if ACF isn't activated
+if(!function_exists('get_field')) {
+  function get_field($field_name, $post_id = 0, $format_value = true) {
+    return false;
+  }
+}
+
+if(!function_exists('have_rows')) {
+  function have_rows($field_name, $post_id = 0) {
+    return false;
+  }
+}
+
+if(!function_exists('the_field')) {
+  function the_field($field_name, $post_id = 0) {
+    echo '';
+    return false;
+  }
+}
