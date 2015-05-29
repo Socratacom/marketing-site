ed65b7b88db15717abe8b6b274c5eae93dfb0e78
Michael Church <michael@fishinglounge.com>
2015-05-29 11:43:49 -0400

    progress dev

--- wp-content/themes/socrata/custom/base.php
+++ wp-content/themes/socrata/custom/base.php
@@ -26,3 +26,30 @@ function no_header() {
     else
     return true;
 }
+
+// CUSTOM LOGO ON LOGIN SCREEN
+add_action('login_head',  'my_custom_login_logo');
+function my_custom_login_logo() {
+	echo '<style type="text/css">h1 a {background-image:url('.get_stylesheet_directory_uri().'/custom/images/admin-logo.png) !important; background-size:274px 63px !important; height:63px !important; width:274px !important;} </style>';
+}
+
+// CUSTOM LOGO ON LOGIN SCREEN LINK
+add_filter('login_headerurl', create_function(false, "return 'http://socrata.com';"));
+
+// CUSTOM LOGO ON LOGIN SCREEN ALT TEXT
+add_filter('login_headertitle', create_function(false, "return 'Socrata';"));
+
+// REMOVE META BOXES FROM WORDPRESS DASHBOARD FOR ALL USERS
+add_action('wp_dashboard_setup', 'example_remove_dashboard_widgets' );
+function example_remove_dashboard_widgets() {
+  global $wp_meta_boxes;	
+	 unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+	 unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+	 unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+}
+
+// DISABLE AUTO SAVE
+add_action( 'wp_print_scripts', 'disableAutoSave' );
+function disableAutoSave(){
+  wp_deregister_script('autosave');
+}
