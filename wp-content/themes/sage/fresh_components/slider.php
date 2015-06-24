c2e79e02dd91b7ad1dacc682f367c10d4825f164
Nuriel Mizrahi <nuriel@freshconsulting.com>
2015-06-24 16:52:19 -0700

    added carousel inside slider

--- wp-content/themes/sage/fresh_components/slider.php
+++ wp-content/themes/sage/fresh_components/slider.php
@@ -14,17 +14,11 @@ function get_slider($i) {
 			$slide_background_color = get_sub_field('slide_background_color') !== '' ? get_sub_field('slide_background_color') : 'white';
 			$slide_background_array = get_sub_field('slide_background_image');
 			$slide_background_image_url = '';
-			if ($slide_background_array) {
-				$slide_background_image_url = $slide_background_array['url'];
-			}
+			$slide_background_image_url = $slide_background_array ? $slide_background_array['url'] : '';
 			$slide_background_image_size = get_sub_field('slide_background_image_size') !== '' && !is_array(get_sub_field('slide_background_image_size')) ? get_sub_field('slide_background_image_size') : 'cover';
 			$slide_background_image_position = get_sub_field('slide_background_image_position') !== ''  && !is_array(get_sub_field('slide_background_image_position')) ? get_sub_field('slide_background_image_position') : 'center center';
 
-			$slide_background_style = '';
-			if ($slide_background_array) {
-				$slide_background_style .= 'background-image: url('. $slide_background_image_url .');';
-			}
-
+			$slide_background_style  = $slide_background_array ? 'background-image: url('. $slide_background_image_url .');' : '';
 			$slide_background_style .= 'background-color: '. $slide_background_color .';
 									    background-position: '. $slide_background_image_position .';
 									    background-size: '. $slide_background_image_size .';
@@ -39,21 +33,61 @@ function get_slider($i) {
 			}
 
 			if (have_rows('rows')) {
-				$rows .= '<div class="row">';
+
 				while (have_rows('rows')) { the_row();
 
+					$activate_carousel = get_sub_field('activate_carousel');
+
+					if ($activate_carousel) {
+						$rows .= '<div class="carousel">';
+					}
+
+					$rows .= '<div class="row">';
+					
 					if (get_sub_field('columns')) {
+
 						$columns_count = count(get_sub_field('columns'));
+						$columns_per_slide = $activate_carousel ? intval(get_sub_field('columns_per_slide')) : 1;
+						$column_number = 0;
+						$col_size = !$activate_carousel ? floor( 12 / $columns_count ) : ( 12 / $columns_per_slide);
+
 						while(have_rows('columns')) { the_row();
+
 							$column_rtp_id = get_sub_field('rtp_id');
-							$rows .= '<div id="'.$column_rtp_id.'" class="col-sm-'. ( 12 / $columns_count ) .'">';
+							
+							$rows .= '<div id="'.$column_rtp_id.'" class="col-sm-'. $col_size .'">';
 							$rows .= get_sub_field('column');
 							$rows .= '</div>';
+
+							if ( $column_number > 0 && $columns_per_slide % $column_number == 1 ) {
+								$rows .= '</div><div class="row">';
+							}
+
+							$column_number++;
+
 						}
 					}
 
+					$rows .= '</div>';
+
+					if ($activate_carousel) {
+						$rows .= "</div><script>
+								$(function(){
+							        $('.slider-$i .carousel').slick({
+										arrows: true,
+										prevArrow: '<i class=\"fa slick-prev fa-chevron-left\"></i>',
+										nextArrow: '<i class=\"fa slick-next fa-chevron-right\"></i>',
+										autoplay: true,
+										autoplaySpeed: 5000,
+										speed: 800,
+										slidesToShow: 1,
+										slidesToScroll: 1
+							        });
+							    });
+							</script>";
+					}
+
 				}
-				$rows .= '</div>';
 			}
 
 			if (get_sub_field('slide_bottom')) {
@@ -80,21 +114,6 @@ function get_slider($i) {
 		return false;
 	}
 
-	$script = "<script>
-	$(function(){
-        $('.slider-$i .slide-list').slick({
-			arrows: true,
-			prevArrow: '<i class=\"fa slick-prev fa-chevron-left\"></i>',
-			nextArrow: '<i class=\"fa slick-next fa-chevron-right\"></i>',
-			autoplay: true,
-			autoplaySpeed: 5000,
-			speed: 800,
-			slidesToShow: 1,
-			slidesToScroll: 1
-        });
-    });
-	</script>";
-
 	echo '<div class="section slider slider-'. $i .'" style="margin:0 -15px; '. ( $slider_fixed_height ? 'height:'. $slider_fixed_height .'px;' : null ) .'">';
 	echo '<div class="slide-list">';
 	echo $slide;
@@ -117,8 +136,21 @@ function get_slider($i) {
 	adjustSectionHeight();
 	</script>";
 
-	if ($slide_number > 1) {
-		echo $script;
+	if ($slide_number > 1) { 
+	echo "<script>
+			$(function(){
+		        $('.slider-$i .slide-list').slick({
+					arrows: true,
+					prevArrow: '<i class=\"fa slick-prev fa-chevron-left\"></i>',
+					nextArrow: '<i class=\"fa slick-next fa-chevron-right\"></i>',
+					autoplay: true,
+					autoplaySpeed: 5000,
+					speed: 800,
+					slidesToShow: 1,
+					slidesToScroll: 1
+		        });
+		    });
+		</script>";
 	}
 
 }
