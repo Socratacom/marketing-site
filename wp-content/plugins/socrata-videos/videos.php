cc1b0d9a19acabb2e6a394cdb18c6d66e8f77fdb
Michael Church <michael@fishinglounge.com>
2015-11-12 16:56:32 -0500

    progress dev

--- wp-content/plugins/socrata-videos/videos.php
+++ wp-content/plugins/socrata-videos/videos.php
@@ -349,3 +349,35 @@ function socrata_videos_posts($atts, $content = null) {
   return $content;
 }
 add_shortcode('socrata-videos-posts', 'socrata_videos_posts');
+
+
+// YouTube Button and Shorcode for TinyMCE
+add_shortcode("youtube", "cwc_youtube");
+function cwc_youtube($atts) {
+  extract(shortcode_atts(array(
+    "id" => '',
+  ), $atts));
+  return '<div class="video-container">
+  <iframe src="http://www.youtube.com/embed/'.$id.'?rel=0" frameborder="0" allowfullscreen></iframe>
+  </div>'
+  ;
+}
+
+add_action('init', 'add_youtube_button');
+function add_youtube_button() {
+   if ( current_user_can('edit_posts') &&  current_user_can('edit_pages') )
+   {
+     add_filter('mce_external_plugins', 'add_youtube_plugin');
+     add_filter('mce_buttons', 'register_youtube_button');
+   }
+}
+
+function register_youtube_button($buttons) {
+   array_push($buttons, "youtube");
+   return $buttons;
+}
+
+function add_youtube_plugin($plugin_array) {
+   $plugin_array['youtube'] = plugins_url( '/js/youtube.js' , __FILE__ );
+   return $plugin_array;
+}
