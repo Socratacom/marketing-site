0274f39bc1e34fd2a671b00318961aefdc295cc4
Michael Church <michael@fishinglounge.com>
2015-12-16 10:44:40 -0500

    update video player

--- wp-content/plugins/socrata-videos/single-videos.php
+++ wp-content/plugins/socrata-videos/single-videos.php
@@ -4,7 +4,8 @@
       <?php while (have_posts()) : the_post(); ?>
       <article <?php post_class(); ?>>
         <div class="video-container">
-          <iframe width="853" height="480" src="https://www.youtube.com/embed/<?php $meta = get_socrata_videos_meta(); echo $meta[1]; ?>?rel=0&amp;showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+          <!--<iframe class="delayed" width="853" height="480" src="https://www.youtube.com/embed/<?php $meta = get_socrata_videos_meta(); echo $meta[1]; ?>?rel=0&amp;showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>-->
+          <div id="ytplayer"></div>
         </div>
         <div class="row">
           <div class="col-sm-10 col-sm-offset-1">
@@ -19,4 +20,29 @@
       <?php endwhile; ?>
     </div>
   </div>    
-</div>
+</div>  
+<script>
+ // Load the IFrame Player API code asynchronously.
+    setTimeout(function() {
+        player.playVideo();
+    }, 3000);
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/player_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+    var player;
+    function onYouTubePlayerAPIReady() {    
+        player = new YT.Player('ytplayer', {
+          height: '480',
+          width: '853',
+          videoId: <?php $meta = get_socrata_videos_meta(); echo "'$meta[1]'"; ?>,
+          playerVars: {
+                 'rel' : 0,
+                 'showinfo' : 0,
+          }
+        }); 
+    }
+</script>
