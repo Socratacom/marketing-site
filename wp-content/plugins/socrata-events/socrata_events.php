674bebff3a2ac0f242db20c7ff2e3d392ddb8fb3
Michael Church <michael@fishinglounge.com>
2016-01-08 17:45:58 -0500

    progress dev

--- wp-content/plugins/socrata-events/socrata_events.php
+++ wp-content/plugins/socrata-events/socrata_events.php
@@ -24,28 +24,89 @@ function events_the_categories() {
 function tribe_events_posts($atts, $content = null) {
   ob_start();
   ?>
-  <section class="section-padding">
+  <section class="section-padding background-clouds featured">
     <div class="container">
-      <div class="row no-gutters">
-        <div class="col-sm-6">
-          <div class="background-sun-flower padding-30" style="height:400px;">
-            <p>test</p>
-          </div>
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <h1 class="text-center margin-bottom-15">Socrata Events</h1>
+          <h3 class="text-center margin-bottom-60">Donec id elit non mi porta gravida at eget metus. Cras mattis consectetur purus sit amet fermentum.</h3>
         </div>
         <div class="col-sm-6">
-          <div class="background-midnight-blue padding-30" style="height:400px;">
-            <p>test</p>
+
+          <div class="img-background" style="background-image: url(/wp-content/uploads/webinar-police.jpg); margin-bottom:30px;">
+            <div class="box-black padding-30" style="min-height:350px;">
+              <h3 class="text-reverse margin-bottom-15">Lunch &amp; Learn</h3>
+              <p class="text-reverse">Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+              <dl>
+                <dt class="text-reverse">Next Lunch &amp; Learn:</dt>
+                <?php
+                $args = array(
+                'post_type' => 'tribe_events',
+                'tribe_events_cat' => 'lunch-and-learn',
+                'posts_per_page' => 1
+                );
+                $query = new WP_Query( $args );
+
+                // The Loop
+                while ( $query->have_posts() ) {
+                $query->the_post(); ?>
+                <?php echo tribe_events_event_schedule_details( $event_id, '<dd class="text-reverse">', '</dd>' ); ?>
+                <?php
+                }
+                ?>
+              </dl>
+              <p><a href="/events/lunch-and-learn" class="btn btn-primary btn-lg">View All <i class="fa fa-arrow-circle-o-right"></i></a></p>
+            </div>
           </div>
+
+        </div>
+        <div class="col-sm-6">
+
+          <div class="img-background" style="background-image: url(/wp-content/uploads/webinar-police.jpg); margin-bottom:30px;">
+            <div class="box-black padding-30" style="min-height:350px;">
+              <h3 class="text-reverse margin-bottom-15">Webinars</h3>
+              <p class="text-reverse">Maecenas faucibus mollis interdum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+              <dl>
+                <dt class="text-reverse">Next Webinar:</dt>
+                <?php
+                $args = array(
+                'post_type' => 'tribe_events',
+                'tribe_events_cat' => 'webinar',
+                'posts_per_page' => 1
+                );
+                $query = new WP_Query( $args );
+
+                // The Loop
+                while ( $query->have_posts() ) {
+                $query->the_post(); ?>
+                <?php echo tribe_events_event_schedule_details( $event_id, '<dd class="text-reverse">', '</dd>' ); ?>
+                <?php
+                }
+                ?>
+              </dl>
+              <p><a href="/events/webinars" class="btn btn-primary btn-lg">View All <i class="fa fa-arrow-circle-o-right"></i></a></p>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
   </section>
-  <section>
+  <section class="section-padding">
     <div class="container">
       <div class="row">
         <div class="col-sm-8">
           <h3>Upcoming Events</h3>
-          <ul>
+
+          <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Event Type <span class="caret"></span></button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+              <li><a href="/events/webinars">Webinar</a></li>
+              <li><a href="/events/lunch-and-learn">Lunch and Learn</a></li>
+            </ul>
+          </div>
+
+          <ul class="event-list">
 
           <?php
             $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
@@ -60,77 +121,72 @@ function tribe_events_posts($atts, $content = null) {
             $query->the_post(); ?>
 
               <?php
-                if( has_term( 'webinars','tribe_events_cat' ) ) { ?>
+                if( has_term( 'webinar','tribe_events_cat' ) ) { ?>
                   <li>
-                    <div><small><?php events_the_categories(); ?></small></div>
-                    <h4><?php the_title(); ?></h4>
-                    <?php echo tribe_events_event_schedule_details( $event_id, '<p>', '</p>' ); ?>
+                    <p class="categories"><?php events_the_categories(); ?></p>
+                    <?php $meta = get_marketo_meta(); 
+                      if ($meta[0]) { ?>
+                        <h4><a href="<?php echo $meta[0];?>" target="_blank"><?php the_title(); ?></a></h4>
+                        <?php
+                      }
+                      else { ?>
+                        <h4><?php the_title(); ?></h4>
+                      <?php
+                      }
+                    ?>                    
+                    <?php echo tribe_events_event_schedule_details( $event_id, '<p class="date">', '</p>' ); ?>
                     <?php the_excerpt(); ?>
                   </li>
                 <?php
                 }
                 elseif ( has_term( 'lunch-and-learn','tribe_events_cat' ) ) { ?>
                   <li>
-                    <div><small><?php events_the_categories(); ?></small></div>
-                    <h4><?php the_title(); ?></h4>
-                    <?php echo tribe_events_event_schedule_details( $event_id, '<p>', '</p>' ); ?>
+                    <p class="categories"><?php events_the_categories(); ?></p>
+                    <?php $meta = get_marketo_meta(); 
+                      if ($meta[0]) { ?>
+                        <h4><a href="<?php echo $meta[0];?>" target="_blank"><?php the_title(); ?></a></h4>
+                        <?php
+                      }
+                      else { ?>
+                        <h4><?php the_title(); ?></h4>
+                      <?php
+                      }
+                    ?>
+                    <?php echo tribe_events_event_schedule_details( $event_id, '<p class="date">', '</p>' ); ?>
                     <?php the_excerpt(); ?>
                   </li>
                 <?php
                 }
                 else { ?>
                   <li>
-                    <div><small><?php events_the_categories(); ?></small></div>
+                    <p class="categories"><?php events_the_categories(); ?></p>
                     <h4><?php the_title(); ?></h4>
-                    <?php echo tribe_events_event_schedule_details( $event_id, '<p>', '</p>' ); ?>
+                    <?php echo tribe_events_event_schedule_details( $event_id, '<p class="date">', '</p>' ); ?>
                     <?php the_excerpt(); ?>
                   </li>
                 <?php
                 }
               ?>
             <?php
-            }
-
-          // Pagination
-          if (function_exists("pagination")) {pagination($query->max_num_pages,$pages);} 
+            }        
 
           // Restore original Post Data
           wp_reset_postdata();
-
           ?>
           </ul>
+          <?php if (function_exists("pagination")) {pagination($query->max_num_pages,$pages);}; ?> 
         </div>      
 
-<div class="col-sm-4">
-<?php
-//list terms in a given taxonomy using wp_list_categories  (also useful as a widget)
-$orderby = 'name';
-$show_count = 0; // 1 for yes, 0 for no
-$pad_counts = 0; // 1 for yes, 0 for no
-$hide_empty = 1;
-$hierarchical = 1; // 1 for yes, 0 for no
-$taxonomy = 'tribe_events_cat';
-$title = 'Event Categories';
+        <div class="col-sm-4">
+          <div class="padding-15 background-clouds">
+            <h4>Let's meet up.</h4>
+            <p>See an event you'd like to attend or want to suggest an event we should attend... drop us a line.</p>
+          </div>
+        </div>
 
-$args = array(
-'orderby' => $orderby,
-'show_count' => $show_count,
-'pad_counts' => $pad_counts,
-'hide_empty' => $hide_empty,
-'hierarchical' => $hierarchical,
-'taxonomy' => $taxonomy,
-'title_li' => '<h5 class="background-wisteria">'. $title .'</h5>'
-);
-?>
-<ul class="category-nav">
-<?php wp_list_categories($args); ?>
-</ul>
-<?php echo do_shortcode('[newsletter-sidebar]'); ?>
-</div>
-</div>
-</div>
-
-</section>
+      </div>
+    </div>
+  </section>
 
 
   <?php
@@ -186,29 +242,7 @@ function events_lunch_learn_posts($atts, $content = null) {
         </div>      
       </div>
       <div class="col-sm-3">
-        <?php
-          //list terms in a given taxonomy using wp_list_categories  (also useful as a widget)
-          $orderby = 'name';
-          $show_count = 0; // 1 for yes, 0 for no
-          $pad_counts = 0; // 1 for yes, 0 for no
-          $hide_empty = 1;
-          $hierarchical = 1; // 1 for yes, 0 for no
-          $taxonomy = 'tribe_events_cat';
-          $title = 'Event Categories';
-
-          $args = array(
-            'orderby' => $orderby,
-            'show_count' => $show_count,
-            'pad_counts' => $pad_counts,
-            'hide_empty' => $hide_empty,
-            'hierarchical' => $hierarchical,
-            'taxonomy' => $taxonomy,
-            'title_li' => '<h5 class="background-wisteria">'. $title .'</h5>'
-          );
-        ?>
-        <ul class="category-nav">
-          <?php wp_list_categories($args); ?>
-        </ul>
+        
         <?php echo do_shortcode('[newsletter-sidebar]'); ?>
       </div>
     </div>
