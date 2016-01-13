969b7fceeb56345cd9ffe6f82c3a02637eaa3cb9
Michael Church <michael@fishinglounge.com>
2016-01-13 14:43:53 -0500

    progress dev

--- wp-content/plugins/socrata-events/socrata_events.php
+++ wp-content/plugins/socrata-events/socrata_events.php
@@ -175,6 +175,25 @@ function tribe_events_posts($atts, $content = null) {
                   </li>
                 <?php
                 }
+                elseif ( has_term( 'conference','tribe_events_cat' ) ) { ?>
+                  <li>
+                    <p class="categories"><?php events_the_categories(); ?></p>
+                    <?php $meta = get_marketo_meta(); 
+                      if ($meta[0]) { ?>
+                        <h4><a href="<?php echo $meta[0];?>" target="_blank"><?php the_title(); ?></a></h4>
+                        <?php
+                      }
+                      else { ?>
+                        <h4><?php the_title(); ?></h4>
+                      <?php
+                      }
+                    ?>
+                    <?php echo tribe_events_event_schedule_details( $event_id, '<p class="date">', '</p>' ); ?>
+                    <?php the_excerpt(); ?>
+                    <p style="margin-top:15px;"><a href="mailto:events@socrata.com" class="btn btn-primary" target="_blank">Meet Us</a></p>
+                  </li>
+                <?php
+                }
                 else { ?>
                   <li>
                     <p class="categories"><?php events_the_categories(); ?></p>
