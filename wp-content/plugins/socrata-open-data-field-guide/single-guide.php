1fdd5ac09e894aeabf892d3c645b37aea0fe799c
Michael Church <michael@fishinglounge.com>
2016-01-15 16:40:30 -0500

    progress dev

--- wp-content/plugins/socrata-open-data-field-guide/single-guide.php
+++ wp-content/plugins/socrata-open-data-field-guide/single-guide.php
@@ -1,28 +1,21 @@
-<div class="container page-padding">
-  <div class="row">
-    <div class="col-sm-7 col-sm-offset-1">
-
-
-      <h1><?php the_title(); ?></h1>
-      <?php echo do_shortcode( '[marketo-share]' ); ?>
-      <hr/>
-      <?php the_content(); ?>
-      <hr/>
-       <div>
-            <?php if( get_posts() ) {
-            previous_post_link('<p><strong><small>NEXT CHAPTER:</small><br>%link</strong></p>');
-            next_post_link('<p><strong><small>PREVIOUS CHAPTER:</small><br>%link</strong></p>');
-            }?>
+<section class="section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-9">
+        <?php the_content(); ?>
+        <hr/>
+        <!--<?php if( get_posts() ) {
+        previous_post_link('<p><strong><small>NEXT CHAPTER:</small><br>%link</strong></p>');
+        next_post_link('<p><strong><small>PREVIOUS CHAPTER:</small><br>%link</strong></p>');
+        }?>-->
+      </div>
+      <div class="col-md-3 hidden-sm hidden-xs">
+        <div class="category-nav">
+          <h5 class="background-wet-asphalt" >Guide Chapters</h5>
+          <?php wp_nav_menu( array( 'theme_location' => 'field_guide' ) ); ?>
         </div>
+        <?php echo do_shortcode('[newsletter-sidebar]'); ?> 
+      </div>
     </div>
-    <div class="col-md-3 hidden-xs">
-      <div class="category-nav">
-      <h5 class="background-wet-asphalt" >Guide Chapters</h5>
-    
-        <?php wp_nav_menu( array( 'theme_location' => 'field_guide' ) ); ?>
-        </div>
-      <?php echo do_shortcode('[newsletter-sidebar]'); ?> 
-    </div>
-
   </div>
-</div>
+</section>
