e895632aae73826f4282561ec4f07848d6190364
Michael Church <michael@fishinglounge.com>
2016-02-01 15:27:43 -0500

    progress dev

--- wp-content/plugins/socrata-events/single-events.php
+++ wp-content/plugins/socrata-events/single-events.php
@@ -1,3 +1,15 @@
+<?php
+$displaydate = rwmb_meta( 'socrata_events_displaydate' );
+$location = rwmb_meta( 'socrata_events_location' );
+$address = rwmb_meta( 'socrata_events_address' );
+$city = rwmb_meta( 'socrata_events_city' );  
+$state = rwmb_meta( 'socrata_events_state' );  
+$zip = rwmb_meta( 'socrata_events_zip' );
+$directions = rwmb_meta( 'socrata_events_directions' );
+$speakers = rwmb_meta( 'socrata_events_speakers' );
+$today = strtotime('today UTC');
+$date = rwmb_meta( 'socrata_events_endtime' );
+?>
 <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); $url = $thumb['0']; ?>
 <section id="top" class="events-hero section-padding background-wet-asphalt">
 <div class="outer">
@@ -8,14 +20,10 @@
         <div class="col-sm-10 col-sm-offset-1">
           <h1 class="text-reverse text-center margin-bottom-15"><?php the_title(); ?></h1>
           <?php
-            $displaydate = rwmb_meta( 'socrata_events_displaydate' );
-            $city = rwmb_meta( 'socrata_events_city' );
-            $state = rwmb_meta( 'socrata_events_state' );
-            $directions = rwmb_meta( 'socrata_events_directions' );
             if ($directions) { ?>
             <ul class="date-location">
               <li class="text-reverse lead"><?php echo $displaydate;?></li>
-              <li class="text-reverse lead"><a href="<?php echo $directions;?>" target="_blank"><?php echo $city;?>, <?php echo $state;?></a></li>
+              <li class="text-reverse lead"><a href="<?php echo $directions;?>" target="_blank"><?php echo $location;?>, <?php echo $city;?>, <?php echo $state;?></a></li>
             </ul>
             <?php
             }
@@ -27,8 +35,7 @@
             <?php
             } 
           ?>
-          <?php
-            $speakers = rwmb_meta( 'socrata_events_speakers' );
+          <?php            
             if ($speakers) { ?>
             <ul class="cta-buttons">
               <li><a href="#registration" target="_blank" class="btn btn-primary btn-lg">Register For Event</a></li>
@@ -38,7 +45,7 @@
             }
             else { ?>
             <ul class="cta-buttons">
-              <li><a href="#registration" target="_blank" class="btn btn-primary">Register For Event</a></li>
+              <li><a href="#registration" target="_blank" class="btn btn-primary btn-lg">Register For Event</a></li>
             </ul>
             <?php
             } 
@@ -49,23 +56,14 @@
   </div>
 
 </div>
-
-  <div class="image overlay-black img-background" style="background-image: url(<?=$url?>);"></div>
+<div class="image overlay-black img-background" style="background-image: url(<?=$url?>);"></div>
 </section>
 <?php echo rwmb_meta( 'socrata_events_wysiwyg' );?>
 <?php
-  $speakers = rwmb_meta( 'socrata_events_speakers' );
   if ($speakers) { ?>
-  <section id="speakers" class="section-padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <h2 class="text-center margin-bottom-60">Meet the Speakers</h2>
-        </div>
-      </div>
-      <?php echo $speakers;?>
-    </div>
-  </section>
+  <div id="speakers">    
+    <?php echo $speakers;?>
+  </div>
   <?php
   }
 ?>
@@ -75,20 +73,13 @@
       <div class="col-sm-5 col-sm-offset-1">
         <h2 class="margin-bottom-15">Register for:</h2>
         <h3 class="margin-bottom-15"><?php the_title(); ?></h3>
-        <?php
-          $displaydate = rwmb_meta( 'socrata_events_displaydate' );            
+        <?php          
           if ($displaydate) { ?>
           <p class="lead"><?php echo $displaydate;?></p>
           <?php
           }
         ?>
-        <?php
-          $location = rwmb_meta( 'socrata_events_location' );
-          $address = rwmb_meta( 'socrata_events_address' );
-          $city = rwmb_meta( 'socrata_events_city' );  
-          $state = rwmb_meta( 'socrata_events_state' );  
-          $zip = rwmb_meta( 'socrata_events_zip' );
-          $directions = rwmb_meta( 'socrata_events_directions' );                
+        <?php                       
           if ($directions) { ?>
           <p class="margin-bottom-15"><?php echo $location;?><br><?php echo $address;?><br><?php echo $city;?>, <?php echo $state;?> <?php echo $zip;?></p>
           <p><a href="<?php echo $directions;?>" target="_blank"><i class="fa fa-map-marker"></i> Get Directions</a></p>
@@ -102,9 +93,7 @@
       </div>
       <div class="col-sm-5">
         <div class="marketo-form">
-          <?php
-          $today = strtotime('today UTC');
-          $date = rwmb_meta( 'socrata_events_endtime' );
+          <?php          
           if ($date < $today) { ?>
           <div class="alert alert-info" style="margin:0;"><strong>This is a past event.</strong> Registration is closed.</div>
           <?php
