de7e4021f7d44aebc326d5b1e3e8668edf812ed7
Michael Church <michael@fishinglounge.com>
2016-02-15 11:48:48 -0500

    progress dev

--- wp-content/themes/sage/templates/content-search.php
+++ wp-content/themes/sage/templates/content-search.php
@@ -2,10 +2,20 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
-				<gcse:searchbox enableHistory="true" autoCompleteMaxCompletions="5" autoCompleteMatchType='any'>
-				<gcse:searchresults refinementStyle="link">
+				<gcse:search></gcse:search>
 			</div>
 		</div>
 	</div>
 </section>
-
+<script>
+(function() {
+var cx = '010927318898930298786:p7-vwztjtlq';
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+    '//cse.google.com/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gcse, s);
+})();
+</script>
