bb827c1e35eb466573bb856b9cf7cd9fe1d7b5fa
Michael Church <michael@fishinglounge.com>
2016-02-15 11:46:06 -0500

    progress dev

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -42,3 +42,15 @@
 		</div>
 	</div>
 </footer>
+<script>
+  (function() {
+    var cx = '010927318898930298786:p7-vwztjtlq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+  </script>
