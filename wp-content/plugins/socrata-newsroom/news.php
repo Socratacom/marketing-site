5bde1c29a025cbd6faa532a4611058d28509c917
Michael Church <michael@fishinglounge.com>
2016-05-02 17:58:42 -0400

    progress dev

--- wp-content/plugins/socrata-newsroom/news.php
+++ wp-content/plugins/socrata-newsroom/news.php
@@ -10,8 +10,6 @@ License: GPLv2
 */
 
 
-
-
 // REGISTER POST TYPE
 add_action( 'init', 'news_post_type' );
 
@@ -35,7 +33,7 @@ function news_post_type() {
       ),
       'public' => true,
       'menu_position' => 5,
-      'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'revisions' ),
+      'supports' => array( 'title','thumbnail' ),
       'taxonomies' => array( '' ),
       'menu_icon' => '',
       'has_archive' => true,
@@ -71,6 +69,9 @@ function create_news_taxonomies() {
     'show_ui' => true,
     'show_tagcloud' => false,
     'hierarchical' => true,
+    'sort' => true,      
+    'args' => array( 'orderby' => 'term_order' ),
+    'show_admin_column' => true,
     'rewrite' => array('with_front' => false, 'slug' => 'newsroom')
     )
   );
@@ -121,126 +122,273 @@ function news_body_class( $classes ) {
   return $classes;
 }
 
+
+// Metabox
+
+add_filter( 'rwmb_meta_boxes', 'news_register_meta_boxes' );
+function news_register_meta_boxes( $meta_boxes )
+{
+  $prefix = 'news_';
+
+  $meta_boxes[] = array(
+    'title'  => __( 'News Details', 'news' ),
+    'post_types' => array( 'news' ),
+    'context'    => 'normal',
+    'priority'   => 'high',
+    'fields' => array(
+      // HEADING
+      array(
+        'type' => 'heading',
+        'name' => __( 'Featured News', 'news' ),
+        'id'   => 'fake_id', // Not used but needed for plugin
+      ),
+      // CHECKBOX
+      array(
+        'name' => __( 'Featured', 'news' ),
+        'id'   => "{$prefix}featured",
+        'desc' => __( 'Yes. This is a featured article.', 'news' ),
+        'type' => 'checkbox',
+        // Value can be 0 or 1
+        'std'  => 0,
+      ),
+      // HEADING
+      array(
+        'type' => 'heading',
+        'name' => __( 'Source', 'news' ),
+        'id'   => 'fake_id', // Not used but needed for plugin
+      ),
+      // TEXT
+      array(
+        'name'  => __( 'Source', 'news' ),
+        'id'    => "{$prefix}source",
+        'desc' => __( 'Eample: Wall Street Journal', 'news' ),
+        'type'  => 'text',
+      ),
+      // URL
+      array(
+        'name' => __( 'Source URL', 'news' ),
+        'id'   => "{$prefix}url",
+        'desc' => __( 'Include the http:// or https://', 'news' ),
+        'type' => 'url',
+      ),
+      // IMAGE ADVANCED (WP 3.5+)
+      array(
+        'name'             => __( 'Logo', 'news' ),
+        'id'               => "{$prefix}logo",
+        'desc' => __( 'NOT FOR PRESS RELEASES', 'news' ),
+        'type'             => 'image_advanced',
+        'max_file_uploads' => 1,
+      ),
+      // HEADING
+      array(
+        'type' => 'heading',
+        'name' => __( 'Content', 'news' ),
+        'id'   => 'fake_id', // Not used but needed for plugin
+      ),
+      array(
+        'name'    => __( 'Press Release Content', 'news' ),
+        'id'      => "{$prefix}wysiwyg",
+        'type'    => 'wysiwyg',
+        // Set the 'raw' parameter to TRUE to prevent data being passed through wpautop() on save
+        'raw'     => false,
+        // Editor settings, see wp_editor() function: look4wp.com/wp_editor
+        'options' => array(
+          'textarea_rows' => 15,
+          'teeny'         => true,
+          'media_buttons' => false,
+        ),
+      ),
+    )
+  );
+  return $meta_boxes;
+}
+
+
 // Shortcode [newsroom-posts]
 function newsroom_posts($atts, $content = null) {
   ob_start();
   ?>
+  <section class="section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
 
-  <div class="container page-padding">
-    <div class="row">
-      <div class="col-sm-9">
-        <div class="row">
 
-          <?php
+<?php
 
-          $do_not_duplicate = array();
 
-          // The Query
-          $args = array(
-                'post_type' => 'news',
-                'posts_per_page' => 1
-              );
-          $query1 = new WP_Query( $args );
+echo '<h3>Featured News</h3>';
+echo '<div class="row">';
 
-          // The Loop
-          while ( $query1->have_posts() ) {
-            $query1->the_post();
-            $do_not_duplicate[] = get_the_ID(); ?>
-            <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'post-image' ); $url = $thumb['0']; ?>
 
-            <div class="col-sm-12">
-              <div class="featured-post overlay-black" style="background-image: url(<?=$url?>);">
-                <div class="text truncate">
-                  <div class="post-category background-alizarin">Newsroom</div>
-                  <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                </div>
-                <p class="meta"><strong>Posted</strong>, <?php the_time('F j, Y') ?></p>
-                <a href="<?php the_permalink() ?>" class="link"></a>
-              </div>
-            </div>
+$args1 = array(
+  'post_type' => 'news',
+  'meta_query' => array(
+    array(
+        'key' => 'news_featured',
+        'value' => '1'
+    )
+  ),
+  'posts_per_page' => 3
+);
+$query1 = new WP_Query( $args1 );
 
+// The Loop
+while ( $query1->have_posts() ) {
+  $query1->the_post();
+  $logo = rwmb_meta( 'news_logo', 'size=medium' );
+  $link = rwmb_meta( 'news_url' );
+  $source = rwmb_meta( 'news_source' );
+  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'post-thumbnail' ); $url = $thumb['0'];
+  $do_not_duplicate[] = get_the_ID(); { ?>
+
+  <?php if ( ! empty( $logo ) ) { ?>
+    <div class="col-sm-4">
+      <article>
+        <div class="sixteen-nine">
+          <div class="aspect-content logo-background" style="background-image:url(<?php foreach ( $logo as $image ) { echo $image['url']; } ?>);"></div>
+          <a href="<?php echo $link;?>" target="_blank" class="link"></a>
+        </div>
+        <div class="text">
+          <p><small><?php news_the_categories(); ?></small></p>
+          <p><a href="<?php echo $link;?>" target="_blank"><?php the_title(); ?></a></p>
+          <p><small><?php echo $source;?> | <?php the_time('F j, Y') ?></small></p>
+        </div>
+      </article>
+    </div>
+    <?php
+    }
+  else { ?>
+    <div class="col-sm-4">
+      <article>
+        <div class="sixteen-nine">
+          <div class="aspect-content post-background" style="background-image:url(<?php echo $url;?>);"></div>
+          <a href="<?php the_permalink() ?>" class="link"></a>
+        </div>
+        <div class="text">
+          <p><small><?php news_the_categories(); ?></small></p>
+          <p><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+          <p><small><?php the_time('F j, Y') ?></small></p>
+        </div>
+      </article>
+    </div>
+  <?php
+    
+  } ?>
+  <?php
+  };
+}
+wp_reset_postdata();
+
+echo '</div>';
+echo '<hr>';
+echo '<h3>Recent News Articles</h3>';
+echo '<ul class="no-bullets news-list">';
+
+/* The 2nd Query (without global var) */
+$args2 = array(
+  'post_type' => 'news',
+  'news_category' => array('socrata-in-the-news','customer-news'),
+  'posts_per_page' => 10,
+  'post__not_in' => $do_not_duplicate 
+);
+$query2 = new WP_Query( $args2 );
+
+// The Loop
+while ( $query2->have_posts() ) {
+  $query2->the_post();
+  $link = rwmb_meta( 'news_url' );
+  $logo = rwmb_meta( 'news_logo', 'size=medium' );
+  $source = rwmb_meta( 'news_source' ); { ?>
+  <li>
+    <div class="thumb">
+      <?php foreach ( $logo as $image ) {
+        echo "<img src='{$image['url']}' width='{$image['width']}' height='{$image['height']}' alt='{$image['alt']}' class='img-responsive' />";
+      } ?>
+    </div>
+    <div class="text">
+      <p><small><?php news_the_categories(); ?></small></p>
+      <p><a href="<?php echo $link;?>" target="_blank"><?php the_title(); ?></a></p>
+      <p><small><?php echo $source;?> | <?php the_time('F j, Y') ?></small></p>
+    </div>
+    <div class="cta-button hidden-xs hidden-sm"><a href="<?php echo $link;?>" target="_blank">Read More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></div>
+  </li>
+  <?php
+  };
+}
+wp_reset_postdata();
+
+echo '</ul>';
+echo '<p><a href="/newsroom/socrata-in-the-news/" class="btn btn-primary margin-bottom-15">View All Socrata in the News <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a> <a href="/newsroom/customer-news/" class="btn btn-primary margin-bottom-15">View All Customers in the News <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>';
+echo '<hr>';
+echo '<h3>Recent Press Releases</h3>';
+echo '<ul class="no-bullets news-list">';
+
+/* The 3rd Query (without global var) */
+$args3 = array(
+  'post_type' => 'news',
+  'news_category' => 'press-releases',
+  'posts_per_page' => 10,
+  'post__not_in' => $do_not_duplicate 
+);
+$query3 = new WP_Query( $args3 );
+
+// The 2nd Loop
+while ( $query3->have_posts() ) {
+  $query3->the_post();
+  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' ); $url = $thumb['0']; { ?>
+  <li>
+    <div class="thumb">
+      <img src="<?php echo $url;?>">
+    </div>
+    <div class="text">
+      <p><small><?php news_the_categories(); ?></small></p>
+      <p><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+      <p><small><?php the_time('F j, Y') ?></small></p>
+    </div>
+    <div class="cta-button hidden-xs hidden-sm"><a href="<?php the_permalink() ?>">Read More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></div>
+  </li>
+  <?php
+  };
+}
+
+// Restore original Post Data
+wp_reset_postdata();
+
+echo '</ul>';
+echo '<p><a href="/newsroom/press-releases/" class="btn btn-primary margin-bottom-15">View All Press Releases <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>';
+
+?>
+        </div>
+        <div class="col-sm-4">
             <?php
-          }
-
-          wp_reset_postdata();
-
-          /* The 2nd Query (without global var) */
-          $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-          $args2 = array(
-                'post_type' => 'news',
-                'paged' => $paged,
-                'post__not_in' => $do_not_duplicate 
-              );
-          $query2 = new WP_Query( $args2 );
-
-          // The 2nd Loop
-          while ( $query2->have_posts() ) {
-            $query2->the_post(); ?>
-            <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'post-thumbnail' ); $url = $thumb['0']; ?>  
-            
-            <div class="col-sm-6 col-lg-4">
-              <div class="card">
-                <div class="card-image hidden-xs">
-                  <?php if ( has_post_thumbnail() ) { ?>
-                    <img src="<?=$url?>" class="img-responsive">
-                  <?php
-                  } else { ?>
-                    <img src="/wp-content/uploads/no-image.png" class="img-responsive">
-                  <?php
-                  }
-                  ?>
-                  <a href="<?php the_permalink() ?>"></a>
-                </div>
-                <div class="card-text truncate">
-                  <p class="categories"><small><?php news_the_categories(); ?><small></p>
-                  <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-                  <p class="meta"><small><strong>Posted</strong>, <?php the_time('F j, Y') ?></small></p>
-                  <?php the_excerpt(); ?> 
-                </div>
-              </div>
-            </div>
-
-            <?php
-          }
-
-          // Pagination
-          if (function_exists("pagination")) {pagination($query2->max_num_pages,$pages);} 
-
-          // Restore original Post Data
-          wp_reset_postdata();
-
+            //list terms in a given taxonomy using wp_list_categories  (also useful as a widget)
+            $orderby = 'name';
+            $show_count = 0; // 1 for yes, 0 for no
+            $pad_counts = 0; // 1 for yes, 0 for no
+            $hide_empty = 1;
+            $hierarchical = 1; // 1 for yes, 0 for no
+            $taxonomy = 'news_category';
+            $title = 'Newsroom Categories';
+            $args = array(
+              'orderby' => $orderby,
+              'show_count' => $show_count,
+              'pad_counts' => $pad_counts,
+              'hide_empty' => $hide_empty,
+              'hierarchical' => $hierarchical,
+              'taxonomy' => $taxonomy,
+              'title_li' => '<h5>'. $title .'</h5>'
+            );
           ?>
-
-        </div>      
-      </div>
-      <div class="col-sm-3">
-        <?php
-          //list terms in a given taxonomy using wp_list_categories  (also useful as a widget)
-          $orderby = 'name';
-          $show_count = 0; // 1 for yes, 0 for no
-          $pad_counts = 0; // 1 for yes, 0 for no
-          $hide_empty = 1;
-          $hierarchical = 1; // 1 for yes, 0 for no
-          $taxonomy = 'news_category';
-          $title = 'Newsroom Categories';
-
-          $args = array(
-            'orderby' => $orderby,
-            'show_count' => $show_count,
-            'pad_counts' => $pad_counts,
-            'hide_empty' => $hide_empty,
-            'hierarchical' => $hierarchical,
-            'taxonomy' => $taxonomy,
-            'title_li' => '<h5>'. $title .'</h5>'
-          );
-        ?>
-        <ul class="category-nav">
-          <?php wp_list_categories($args); ?>
-        </ul>
-        <?php echo do_shortcode('[newsletter-sidebar]'); ?>
+          <ul class="category-nav">
+            <?php wp_list_categories($args); ?>
+          </ul>
+          <?php echo do_shortcode('[newsletter-sidebar]'); ?>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
+
 
   <?php
   $content = ob_get_contents();
--- wp-content/plugins/socrata-newsroom/single-news.php
+++ wp-content/plugins/socrata-newsroom/single-news.php
@@ -1,60 +1,218 @@
 <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'feature-image' ); $url = $thumb['0']; ?>
-<div class="feature-image hidden-xs" style="background-image: url(<?=$url?>);">
-  <div class="pattern-overlay"></div>  
-  <?php echo do_shortcode('[image-attribution]'); ?>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-8 col-md-7 col-md-offset-1 article-content">
-      <div class="wrapper">
-        <?php while (have_posts()) : the_post(); ?>
-          <article <?php post_class(); ?>>
-            <small class="category-name"><?php news_the_categories(); ?></small>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-            <p class="meta"><small><strong>Posted</strong>, <?php the_time('F jS, Y') ?></small></p>
-            <hr/>
-            <div class="entry-content">
-              <?php the_content(); ?>
-            </div>
-            <hr/>
-            <div>
-              <?php if( get_posts() ) {
-              previous_post_link('<p><strong><small>NEXT POST:</small><br>%link</strong></p>');
-              next_post_link('<p><strong><small>PREVIOUS POST:</small><br>%link</strong></p>');
-              }?>
-            </div>
-          </article>
-          <div class="marketo-share">
-            <?php echo do_shortcode( '[marketo-share]' ); ?>
+<div class="hero img-background background-primary-alt-2-light overlay overlay-primary-alt-2" style="background-image: url(<?=$url?>);">
+  <div class="outer">
+    <div class="inner">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-10 col-sm-offset-1">
+            <h1 class="text-center text-reverse"><?php the_title(); ?></h1>
           </div>
-        <?php endwhile; ?>
+        </div>
       </div>
     </div>
-    <div class="col-sm-4 col-md-3 sidebar">
-      <?php
-        //list terms in a given taxonomy using wp_list_categories  (also useful as a widget)
-        $orderby = 'name';
-        $show_count = 0; // 1 for yes, 0 for no
-        $pad_counts = 0; // 1 for yes, 0 for no
-        $hide_empty = 1;
-        $hierarchical = 1; // 1 for yes, 0 for no
-        $taxonomy = 'news_category';
-        $title = 'Newsroom Categories';
+  </div>
+<?php echo do_shortcode('[image-attribution]'); ?>
+</div>
+<section class="content">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">
+        <?php while (have_posts()) : the_post(); ?>
+          <article <?php post_class(); ?>>
+            <div class="social-sharing">
+              <?php echo do_shortcode('[marketo-share-custom]');?>
+            </div>
+            <p class="meta"><small><?php news_the_categories(); ?> | Posted: <?php the_time('F jS, Y') ?></small></p>
+            <?php the_content(); ?>
+          </article>
+        <?php endwhile; ?>
+        <hr/>
+        <div class="row next-prev-posts">
+        <?php
+        $prev_post = get_previous_post(TRUE, '', 'news_category');
+        if (!empty( $prev_post )): ?>
+          <div class="col-sm-6">
+            <div class="margin-bottom-15 thumb"><span>Next Press Release</span><a href="<?php echo get_permalink( $prev_post->ID ); ?>"><?php echo get_the_post_thumbnail($prev_post->ID, 'post-thumbnail', array('class'=>'img-responsive')); ?></a></div>
+            <div class="category-name">
+                <?php 
+                $cats = get_the_category( $prev_post->ID );
+                echo $cats[0]->cat_name;
+                for ($i = 1; $i < count($cats); $i++) {echo ', ' . $cats[$i]->cat_name ;}
+                ?>
+            </div>
+            <h5><a href="<?php echo get_permalink( $prev_post->ID ); ?>"><?php echo get_the_title( $prev_post->ID ); ?></a></h5>
+            <p><small><?php echo get_the_time( 'F j, Y', $prev_post->ID ); ?></small></p>
+          </div>
+        <?php endif; ?>
+        <?php
+        $next_post = get_next_post(TRUE, '', 'news_category');
+        if (!empty( $next_post )): ?>
+          <div class="col-sm-6">
+            <div class="margin-bottom-15 thumb"><span>Previous Press Release</span><a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo get_the_post_thumbnail($next_post->ID, 'post-thumbnail', array('class'=>'img-responsive')); ?></a></div>
+            <div class="category-name">
+                <?php 
+                $cats = get_the_category( $next_post->ID );
+                echo $cats[0]->cat_name;
+                for ($i = 1; $i < count($cats); $i++) {echo ', ' . $cats[$i]->cat_name ;}
+                ?>
+            </div>
+            <h5><a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo get_the_title( $next_post->ID ); ?></a></h5>
+            <p><small><?php echo get_the_time( 'F j, Y', $next_post->ID ); ?></small></p>
+          </div>
+        <?php endif; ?>
+        </div>
+      </div>
+      <div class="col-sm-4 hidden-xs">
+        <?php
+          //list terms in a given taxonomy using wp_list_categories  (also useful as a widget)
+          $orderby = 'name';
+          $show_count = 0; // 1 for yes, 0 for no
+          $pad_counts = 0; // 1 for yes, 0 for no
+          $hide_empty = 1;
+          $hierarchical = 1; // 1 for yes, 0 for no
+          $taxonomy = 'news_category';
+          $title = 'Newsroom Categories';
 
-        $args = array(
-          'orderby' => $orderby,
-          'show_count' => $show_count,
-          'pad_counts' => $pad_counts,
-          'hide_empty' => $hide_empty,
-          'hierarchical' => $hierarchical,
-          'taxonomy' => $taxonomy,
-          'title_li' => '<h5>'. $title .'</h5>'
-        );
-      ?>
-      <ul class="category-nav">
-        <?php wp_list_categories($args); ?>
-      </ul>
-      <?php echo do_shortcode('[newsletter-sidebar]'); ?> 
+          $args = array(
+            'orderby' => $orderby,
+            'show_count' => $show_count,
+            'pad_counts' => $pad_counts,
+            'hide_empty' => $hide_empty,
+            'hierarchical' => $hierarchical,
+            'taxonomy' => $taxonomy,
+            'title_li' => '<h5>'. $title .'</h5>'
+          );
+        ?>
+        <ul class="category-nav">
+          <?php wp_list_categories($args); ?>
+        </ul>
+        <?php echo do_shortcode('[newsletter-sidebar]'); ?> 
+
+
+
+        <?php
+          $args1 = array(
+          'post_type'         => 'news',
+          'news_category'     => 'press-releases',
+          'order'             => 'desc',
+          'posts_per_page'    => 3,
+          'post_status'       => 'publish',
+          );
+
+          // The Query
+          $query1 = new WP_Query( $args1 );
+
+          echo '<ul class="no-bullets sidebar-list">';
+          echo '<li><h5>Recent Press Releases</h5></li>';
+
+          // The Loop
+          if ( $query1->have_posts() ) {          
+          while ( $query1->have_posts() ) {
+          $query1->the_post(); { ?> 
+            <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' ); $url = $thumb['0'];?>
+            <li>
+              <div class="article-img-container">
+                <img src="<?=$url?>" class="img-responsive">
+              </div>
+              <div class="article-title-container">
+                <a href="<?php the_permalink() ?>"><?php the_title(); ?></a><br><small><?php the_time('F j, Y') ?></small>
+              </div>
+            </li>
+            <?php }
+            }          
+          } 
+          else {
+          // no posts found
+          }
+          echo '<li><a href="/newsroom/press-releases/">View All Press Releases <i class="fa fa-arrow-circle-o-right"></i></a></li>';
+          echo '</ul>';
+          /* Restore original Post Data */
+          wp_reset_postdata();
+
+          $args2 = array(
+          'post_type'         => 'news',
+          'news_category'     => 'socrata-in-the-news',
+          'order'             => 'desc',
+          'posts_per_page'    => 3,
+          'post_status'       => 'publish',
+          );
+
+          // The Query
+          $query2 = new WP_Query( $args2 );
+
+          echo '<ul class="no-bullets sidebar-list">';
+          echo '<li><h5>Recent Socrata in the News</h5></li>';
+
+          // The Loop
+          if ( $query2->have_posts() ) {          
+          while ( $query2->have_posts() ) {
+          $query2->the_post();
+          $link = rwmb_meta( 'news_url' ); { ?> 
+            <li>
+              <div class="article-title-container">                
+                <a href="<?php echo $link;?>" target="_blank"><?php the_title(); ?></a><br><small><?php the_time('F j, Y') ?></small>
+              </div>
+            </li>
+            <?php }
+            }          
+          } 
+          else {
+          // no posts found
+          }
+          echo '<li><a href="/newsroom/socrata-in-the-news/">View All Socrata in the News <i class="fa fa-arrow-circle-o-right"></i></a></li>';
+          echo '</ul>';
+          /* Restore original Post Data */
+          wp_reset_postdata();
+
+          $args3 = array(
+          'post_type'         => 'news',
+          'news_category'     => 'customer-news',
+          'order'             => 'desc',
+          'posts_per_page'    => 3,
+          'post_status'       => 'publish',
+          );
+
+          // The Query
+          $query3 = new WP_Query( $args3 );
+
+          echo '<ul class="no-bullets sidebar-list">';
+          echo '<li><h5>Recent Customers in the News</h5></li>';
+
+          // The Loop
+          if ( $query3->have_posts() ) {          
+          while ( $query3->have_posts() ) {
+          $query3->the_post();
+          $link = rwmb_meta( 'news_url' ); { ?> 
+            <li>
+              <div class="article-title-container">                
+                <a href="<?php echo $link;?>" target="_blank"><?php the_title(); ?></a><br><small><?php the_time('F j, Y') ?></small>
+              </div>
+            </li>
+            <?php }
+            }          
+          } 
+          else {
+          // no posts found
+          }
+          echo '<li><a href="/newsroom/customer-news/">View All Customers in the News <i class="fa fa-arrow-circle-o-right"></i></a></li>';
+          echo '</ul>';
+          /* Restore original Post Data */
+          wp_reset_postdata();
+
+        ?>
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
     </div>
   </div>
-</div>
+</section>
