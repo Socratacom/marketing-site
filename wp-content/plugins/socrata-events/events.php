ff42df2619ec88ffe6051531cd1b1aef84069c94
Michael Church <michael@fishinglounge.com>
2016-06-28 14:34:00 -0400

    progress dev

--- wp-content/plugins/socrata-events/events.php
+++ wp-content/plugins/socrata-events/events.php
@@ -581,7 +581,7 @@ function events_map($atts, $content = null) {
               <div class="col-sm-12">
                 <h2 class="text-center text-reverse margin-bottom-15">Join us at these events</h2>
                 <p class="lead text-reverse text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p class="text-center margin-bottom-0"><button class="btn btn-warning map-button">View Map</button></p>
+                <p class="text-center margin-bottom-0"><button class="btn btn-lg btn-warning map-button">Explore Map</button></p>
               </div>
             </div>
           </div>
