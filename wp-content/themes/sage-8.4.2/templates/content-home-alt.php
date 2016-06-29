99f773023c63eb32b3dfea479bb4fcb3e5063318
Michael Church <michael@fishinglounge.com>
2016-06-29 19:42:35 -0400

    progress dev

--- wp-content/themes/sage-8.4.2/templates/content-home-alt.php
+++ wp-content/themes/sage-8.4.2/templates/content-home-alt.php
@@ -1,7 +1,17 @@
-<section class="home-hero">
-	<div class="frame"></div>
+<section class="section-padding home-hero" style="background-image: url(/wp-content/uploads/home-hero-default.jpg);">
+	<div class="container" style="padding-top:100px;">
+		<div class="row">
+			<div class="col-sm-6">
+				<h1 class="margin-bottom-15">The Data Platform for 21st Century Digital Government</h1>
+				<p class="lead color-black">Socrata's cloud-based solution allows government organizations to put their data online, make data-driven decisions, operate more efficiently, and share insights with citizens.</p>
+			</div>
+		</div>
+	</div>
+	<div class="scroll">
+		<div class="mouse"><a href="#start"></a></div>
+	</div>
 </section>
-<section class="section-padding">
+<section id="start" class="section-padding">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-10 col-sm-offset-1">
@@ -9,32 +19,32 @@
 				<p class="lead text-center margin-bottom-60">Socrata provides cloud solutions for federal, state, and local governments to transform data into actionable insights for public and government use.</p>
 			</div>
 			<div class="col-sm-3">
-				<p class="text-center"><i class="padding-15 img-circle background-white color-secondary icon-50 icon-capital"></i></p>
-				<h5 class="text-center text-reverse title-border">Federal Government</h5>
-				<p class="text-reverse">Socrata for Federal Government helps federal departments and agencies maximize the power of their data.</p>
-				<p class="text-reverse"><i class="fa fa-info-circle" aria-hidden="true"></i> <i>In Process for FedRamp certification in 2016</i></p>
-				<p><a href="/solutions/federal-government/" class="btn btn-primary">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
+				<p class="text-center"><i class="color-primary icon-100 icon-capital"></i></p>
+				<h5 class="text-center title-border">Federal Government</h5>
+				<p>Socrata for Federal Government helps federal departments and agencies maximize the power of their data.</p>
+				<p><i class="fa fa-info-circle" aria-hidden="true"></i> <i>In Process for FedRamp certification in 2016</i></p>
+				<p><a href="/solutions/federal-government/">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
 			</div>
 			<div class="col-sm-3">
-				<p class="text-center"><i class="padding-15 img-circle background-white color-secondary icon-50 icon-state"></i></p>
-				<h5 class="text-center text-reverse title-border">State Government</h5>
-				<p class="text-reverse">States rely on Socrata to increase transparency, improve operational efficiencies, and stimulate their economy.</p>
-				<p class="text-reverse"><i class="fa fa-info-circle" aria-hidden="true"></i> <i>25 States use Socrata products and services</i></p>
-				<p><a href="/solutions/state-government/" class="btn btn-primary">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
+				<p class="text-center"><i class="color-primary icon-100 icon-state"></i></p>
+				<h5 class="text-center title-border">State Government</h5>
+				<p>States rely on Socrata to increase transparency, improve operational efficiencies, and stimulate their economy.</p>
+				<p><i class="fa fa-info-circle" aria-hidden="true"></i> <i>25 States use Socrata products and services</i></p>
+				<p><a href="/solutions/state-government/">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
 			</div>
 			<div class="col-sm-3">
-				<p class="text-center"><i class="padding-15 img-circle background-white color-secondary icon-50 icon-map"></i></p>
-				<h5 class="text-center text-reverse title-border">County Government</h5>
-				<p class="text-reverse">With Socrata, counties can publish data that will boost the economy and make the region a better place to live.</p>
-				<p class="text-reverse"><i class="fa fa-info-circle" aria-hidden="true"></i> <i>4,000+ datasets are hosted by counties on Socrata</i></p>
-				<p><a href="/solutions/county-government/" class="btn btn-primary">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
+				<p class="text-center"><i class="color-primary icon-100 icon-map"></i></p>
+				<h5 class="text-center title-border">County Government</h5>
+				<p>With Socrata, counties can publish data that will boost the economy and make the region a better place to live.</p>
+				<p><i class="fa fa-info-circle" aria-hidden="true"></i> <i>4,000+ datasets are hosted by counties on Socrata</i></p>
+				<p><a href="/solutions/county-government/">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
 			</div>
 			<div class="col-sm-3">
-				<p class="text-center"><i class="padding-15 img-circle background-white color-secondary icon-50 icon-city"></i></p>
-				<h5 class="text-center text-reverse title-border">City Government</h5>
-				<p class="text-reverse">Large and small cities can utilize Socrata solutions to communicate more effectively with their constituents and to provide the essential services they rely upon.</p>
-				<p class="text-reverse"><i class="fa fa-info-circle" aria-hidden="true"></i> <i>300+ Cities use Socrata</i></p>
-				<p><a href="/solutions/city-government/" class="btn btn-primary">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
+				<p class="text-center"><i class="color-primary icon-100 icon-city"></i></p>
+				<h5 class="text-center title-border">City Government</h5>
+				<p>Large and small cities can utilize Socrata solutions to communicate more effectively with their constituents and to provide the essential services they rely upon.</p>
+				<p><i class="fa fa-info-circle" aria-hidden="true"></i> <i>300+ Cities use Socrata</i></p>
+				<p><a href="/solutions/city-government/">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></p>
 			</div>
 		</div>
 	</div>
