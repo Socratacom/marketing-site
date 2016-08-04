0309462877268ab9126c6c8f201508528e26586a
Michael Church <michael@fishinglounge.com>
2016-08-04 16:11:02 -0400

    progress dev

--- wp-content/plugins/socrata-events/single-events.php
+++ wp-content/plugins/socrata-events/single-events.php
@@ -62,7 +62,7 @@ $url = $thumb['0'];
       <div class="container">
         <div class="row">
           <div class="col-sm-10 col-sm-offset-1">
-            <div class="padding-bottom-30 margin-bottom-30" style="border-bottom:#ebebeb solid 1px;">
+            <div class="padding-bottom-30 margin-bottom-30 sharing-container" style="border-bottom:#ebebeb solid 1px;">
               <div class="social-sharing-mini"><?php echo do_shortcode('[marketo-share]');?></div>
             </div>
             <?php echo rwmb_meta( 'socrata_events_wysiwyg' );?>
