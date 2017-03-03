9d1c7d0fbbf411d2588b4fc4fad93d4093786a1d
Michael Church <michael@fishinglounge.com>
2017-03-03 16:36:45 -0500

    progress dev

--- wp-content/themes/sage-8.4.2/templates/content-home-takeover.php
+++ wp-content/themes/sage-8.4.2/templates/content-home-takeover.php
@@ -1,48 +1,49 @@
-<section class="section-full img-background-fixed background-primary background-multiply" style="background-image:url(/wp-content/uploads/connect-livestream.jpg) ">
+<section class="section-padding img-background-fixed background-primary background-multiply" style="background-image:url(/wp-content/uploads/connect-livestream.jpg);">
 	
 	<div class="middle">
 
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-6 col-sm-offset-3">
-					<h1 class="margin-bottom-0 color-white">Socrata Connect</h1>
+					<div class="margin-bottom-30"><img src="/wp-content/uploads/homepage-connect-logo.png" class="img-responsive"></div>
 					<h3 class="color-white">Watch the livestream March 6 and 7.</h3>
-
-
-						<form action="http://go.socrata.com/l/303201/2017-02-23/998i" method="post">
-							<div class="row">
-								<div class="coln-sm-12 col-md-6">
-									<div class="form-group">
-									<label class="sr-only">First Name</label><input class="form-control" type="text" name="firstname" required="required" placeholder="First Name" />
-									</div>
-								</div>
-								<div class="col-sm-12 col-md-6">
-									<div class="form-group">
-									<label class="sr-only">Last Name</label><input class="form-control" type="text" name="lastname" required="required" placeholder="Last Name" />
-									</div>
+					<form action="https://go.socrata.com/l/303201/2017-02-23/998i" method="post" class="margin-bottom-60">
+						<div class="row">
+							<div class="coln-sm-12 col-md-6">
+								<div class="form-group">
+								<label class="sr-only">First Name</label><input class="form-control" type="text" name="firstname" required="required" placeholder="First Name" style="border-color:#fff;" />
 								</div>
 							</div>
-							<div class="row">
-								<div class="col-sm-12">
-									<div class="form-group">
-										<label class="sr-only">Email Address</label><input class="form-control" type="email" name="email" required="required" placeholder="Email Address" />
-									</div>
-									<div class="form-group">
-										<label class="sr-only">Company</label><input class="form-control" type="text" name="company" required="required" placeholder="Company" />
-									</div>
-									<div class="form-group">
-										<label class="sr-only">Job Title</label><input class="form-control" type="text" name="jobtitle" required="required" placeholder="Job Title" />
-									</div>
-									<div style="position:absolute; left:-9999px; top: -9999px;">
-									<label for="pardot_extra_field">Comments</label>
-									<input type="text" id="pardot_extra_field" name="pardot_extra_field">
-									</div>
-									<button type="submit" class="btn btn-primary" value="submit" required="required" />Watch now</button>
+							<div class="col-sm-12 col-md-6">
+								<div class="form-group">
+								<label class="sr-only">Last Name</label><input class="form-control" type="text" name="lastname" required="required" placeholder="Last Name" style="border-color:#fff;" />
 								</div>
 							</div>
-						</form>
-
-
+						</div>
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="form-group">
+									<label class="sr-only">Email Address</label><input class="form-control" type="email" name="email" required="required" placeholder="Email Address" style="border-color:#fff;" />
+								</div>
+								<div class="form-group">
+									<label class="sr-only">Company</label><input class="form-control" type="text" name="company" required="required" placeholder="Company" style="border-color:#fff;" />
+								</div>
+								<div class="form-group">
+									<label class="sr-only">Job Title</label><input class="form-control" type="text" name="jobtitle" required="required" placeholder="Job Title" style="border-color:#fff;" />
+								</div>
+								<div style="position:absolute; left:-9999px; top: -9999px;">
+								<label for="pardot_extra_field">Comments</label>
+								<input type="text" id="pardot_extra_field" name="pardot_extra_field">
+								</div>
+								<button type="submit" class="btn btn-primary" value="submit" required="required" />Watch now</button>
+							</div>
+						</div>
+					</form>
+					<p class="color-white text-uppercase">Sponsored by</p>
+					<ul style="list-style-type: none; margin:0; padding:0; display:table;">
+						<li style="display:table-cell; vertical-align: middle; padding-right:20px;"><img src="/wp-content/uploads/grant-thornton-white.png"><li>
+						<li style="display:table-cell; vertical-align: middle;"><img src="/wp-content/uploads/aws-white.png"><li>
+					</ul>
 					
 				</div>
 			</div>
