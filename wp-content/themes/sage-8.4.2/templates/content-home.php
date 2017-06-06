6b9ce372cfc9a4ae72e409bb41f3ce9c64992fa7
Michael Church <michael@fishinglounge.com>
2017-06-06 11:32:23 -0400

    progress dev

--- wp-content/themes/sage-8.4.2/templates/content-home.php
+++ wp-content/themes/sage-8.4.2/templates/content-home.php
@@ -1,15 +1,16 @@
 <section class="home-masthead">
 <div class="slider">
-	<div class="slide" style="background-image:url(/wp-content/uploads/hero-virginia-beach.jpg)">
+	<div class="slide" style="background-image:url(/wp-content/uploads/hero-fedramp.jpg)">
 		<div class="text">
 			<div class="vertical-center">
 				<div class="container">
 					<div class="row">
-						<div class="col-sm-8 col-md-6">
-							<h1 class="color-white margin-bottom-15" style="line-height:1.1em;">A Future Fueled by Data</h1>
-							<h4 class="color-white text-normal hidden-xs margin-bottom-0">What Works City Virginia Beach has set a new standard for transparency and performance measurement with open data</h4>
+						<div class="col-sm-8">
+							<h1 class="color-white margin-bottom-15">Socrata's Government Data Platform Achieves FedRAMP<sup><small class="font-normal color-white">SM</small></sup> Authorization</h1>
+							<h4 class="color-white text-normal hidden-xs margin-bottom-0">Our new FedRAMP-accredited data platform provides agencies with an enterprise-ready environment to securely collect, manage, and analyze data to support delivery of mission-critical programs.</h4>
 							<ul class="cta-list">
-								<li><a href="/case-study/future-fueled-data-virginia-beach/" class="btn btn-default outline">Read More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
+								<li><a href="/newsroom-article/socrata-announces-fedramp-authorized-government-data-platform/" class="btn btn-default outline">Press Release <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
+								<li class="hidden-xs"><a href="/fedramp" class="btn btn-default outline">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
 							</ul>
 						</div>
 					</div>
@@ -17,17 +18,34 @@
 			</div>
 		</div>
 	</div>
-	<div class="slide" style="background-image:url(/wp-content/uploads/firefighter-hero.jpg)">
+	<div class="slide" style="background-image:url(/wp-content/uploads/hero-blueprint.jpg)">
 		<div class="text">
 			<div class="vertical-center">
 				<div class="container">
 					<div class="row">
 						<div class="col-sm-8 col-md-6">
-							<h1 class="color-white margin-bottom-15">First Due Size-Up is Transforming Emergency Response</h1>
-							<h4 class="color-white text-normal hidden-xs margin-bottom-0">Their mission is simple: harness and deploy the power of data to help end the thousands of first responder and civilian injuries and fatalities that are caused each year by a lack of critical up-front information</h4>
+							<h1 class="color-white margin-bottom-15">Socrata Blueprint</h1>
+							<h4 class="color-white text-normal hidden-xs margin-bottom-0">Socrata Blueprint is a roadmap for governments to create a sustainable data program. Blueprint offers best practices for cross-collaboration and better decision-making based on data, all designed to improve citizen’s lives</h4>
 							<ul class="cta-list">
-								<li><a href="/blog/firefighters-can-save-lives-data/" class="btn btn-default outline">Read <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
-								<li><a href="http://firstduesizeup.com/" class="btn btn-default outline" target="_blank">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
+								<li><a href="/blueprint" class="btn btn-default outline">Learn More <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="slide" style="background-image:url(/wp-content/uploads/april-hero-1.jpg)">
+		<div class="text">
+			<div class="vertical-center">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-8 col-md-6">
+							<h1 class="color-white margin-bottom-15">Lead with Data</h1>
+							<h4 class="color-white text-normal hidden-xs margin-bottom-0">Socrata's data-as-a-service platform empowers the most innovative governments make data-driven decisions, operate more efficiently, and share insights with citizens</h4>
+							<ul class="cta-list">
+								<li><a href="/solutins" class="btn btn-default outline">View our Solutions <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
+								<li><a href="/contact-us" class="btn btn-default outline">Contact Sales <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a></li>
 							</ul>
 						</div>
 					</div>
@@ -61,7 +79,7 @@ jQuery(function ($){
 				<h2 class="text-center section-title">Trusted by over 1,000 government organizations</h2>
 			</div>
 			<div class="col-sm-6 col-md-3 match-height margin-bottom-30">
-				<p><a href="/solutions/federal-government"><img src="/wp-content/uploads/thumb-federal.jpg" class="img-responsive"></a></p>
+				<p><a href="/solutions/socrata-for-federal-government"><img src="/wp-content/uploads/thumb-federal.jpg" class="img-responsive"></a></p>
 				<h5 class="text-center">Federal Government</h5>
 			</div>
 			<div class="col-sm-6 col-md-3 match-height margin-bottom-30">
