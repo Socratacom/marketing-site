a574be304fdc71dd33abcfbf32cbe53110cdd337
Michael Church <michael@fishinglounge.com>
2017-07-06 17:23:01 -0400

    progress dev

--- wp-content/plugins/socrata-webinars/single-webinars.php
+++ wp-content/plugins/socrata-webinars/single-webinars.php
@@ -256,22 +256,35 @@ function OnButton2()
 <label for="pardot_extra_field">Comments</label>
 <input type="text" id="pardot_extra_field" name="pardot_extra_field">
 </div>
-<button type="submit" class="btn btn-primary" value="submit" onclick="OnButton1(); OnButton2();" />Register</button>
+<button type="submit" class="btn btn-primary" value="submit" />Register</button>
 </form>
 
 <script language="Javascript">
-function OnButton1()
-{
-    document.myform.action = "https://event.on24.com/utilApp/r"
-    document.myform.submit();
-    return true;
-}
-function OnButton2()
-{
-    document.myform.action = "<?php echo $form_registration ;?>"
-    document.myform.submit();
-    return true;
-}
+$(document).on('click','input[type=submit]',function(e){
+    e.preventDefault();
+    $.ajax({
+        type:'post',
+        url:"<?php echo $form_registration ;?>",
+        data:$('form:eq(0)').serialize(),
+        success:function(resp){
+            alert('first sent');
+        },
+        fail:function(resp){
+            alert('couldn\'t send the first');
+        }
+    });
+    $.ajax({
+        type:'post',
+        url:"https://event.on24.com/utilApp/r",
+        data:$('form:eq(0)').serialize(),
+        success:function(resp){
+            alert('second sent');
+        },
+        fail:function(resp){
+            alert('couldn\'t send the second');
+        }
+    });
+});
 </script>
 
 
