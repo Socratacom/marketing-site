f304c0a8c16de75c8d44e7a8715acb9255dd0b44
Michael Church <michael@fishinglounge.com>
2017-08-18 14:39:42 -0400

    progress dev

--- wp-content/plugins/socrata-media-kit/single-media-kit.php
+++ wp-content/plugins/socrata-media-kit/single-media-kit.php
@@ -1,66 +1,71 @@
 <?php 
-  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'post-image' );
-  $url = $thumb['0'];
-  $img_id = get_post_thumbnail_id(get_the_ID());
-  $alt_text = get_post_meta($img_id , '_wp_attachment_image_alt', true);
-  $quote = rwmb_meta( 'case_study_quote' );
-  $name = rwmb_meta( 'case_study_name' );
-  $title = rwmb_meta( 'case_study_title' );
-  $customer = rwmb_meta( 'case_study_customer' );
-  $site_name = rwmb_meta( 'case_study_site_name' );
-  $site = rwmb_meta( 'case_study_url' );
-  $highlight = rwmb_meta( 'case_study_highlight' );
-  $headshot = rwmb_meta( 'case_study_headshot', 'size=thumbnail' );
-  $content = rwmb_meta( 'case_study_wysiwyg' );
+  $about_content = rwmb_meta( 'media_kit_about_socrata_content' );
 ?>
-<section class="section-padding">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12">
-        <h1 class="font-light text-center"><?php the_title(); ?></h1>
-        <p class="lead margin-bottom-60 text-center">Donec ullamcorper nulla non metus auctor fringilla. Donec sed odio dui.</p>
-      </div>
-
-
-<ul class="nav nav-tabs responsive-tabs">
-  <li class="active"><a data-toggle="tab" href="#home">Coverage</a></li>
-  <li><a data-toggle="tab" href="#menu1">About Socrata</a></li>
-  <li><a data-toggle="tab" href="#menu2">Digital Assets</a></li>
-  <li><a data-toggle="tab" href="#menu3">Socrata Connect</a></li>
-</ul>
-
-<div class="tab-content">
-  <div id="home" class="tab-pane active">
-    <h3>HOME</h3>
-    <p>Some content.</p>
-  </div>
-  <div id="menu1" class="tab-pane">
-    <h3>Menu 1</h3>
-    <p>Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus.</p>
-  </div>
-  <div id="menu2" class="tab-pane">
-    <h3>Menu 2</h3>
-    <p>Some content in menu 2.</p>
-  </div>
-  <div id="menu3" class="tab-pane">
-    <h3>Menu 2</h3>
-    <p>Some content in menu 3.</p>
+<style>
+.essential-links ul li {font-size:14px; border-bottom:#d1d1d1 solid 1px; margin:0; padding:10px 0;}
+</style>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-3 hidden-xs" style="padding-top:80px;">
+      <div id="sidebar" class="background-light-grey-4">
+        <ul>
+          <li><a href="index.html">jQuery (animated)</a></li>
+          <li><a href="#section-2">CSS (fixed)</a></li>
+          <li><a href="reveal.html">CSS (reveal)</a></li>
+        </ul>
+      </div>        
+    </div>
+    <div class="col-sm-9">
+      <!-- Section 1 -->
+      <section class="section-padding">
+        <div class="row">
+          <div class="col-sm-7 col-md-8">
+            <h1 class="font-light"><?php the_title(); ?></h1>
+            <?php echo $about_content;?>          
+          </div>
+          <div class="col-sm-5 col-md-4">
+            <div class="essential-links">
+              <h5 class="text-uppercase">Essential Links</h5>
+              <ul class="no-bullets">
+                <li><span class="text-uppercase font-semi-bold">Press Contact</span><br><a href="mailto:press@socrata.com" class="font-normal">press@socrata.com</a></li>
+                <li><span class="text-uppercase font-semi-bold">Twitter</span><br><a href="https://twitter.com/socrata" target="_blank" class="font-normal">@socrata</a></li>
+                <li><span class="text-uppercase font-semi-bold">CEO Twitter</span><br><a href="https://twitter.com/kmerritt" target="_blank" class="font-normal">@kmerritt</a></li>
+                <li><span class="text-uppercase font-semi-bold">Linked In</span><br><a href="https://www.linkedin.com/company-beta/428169/" target="_blank" class="font-normal">View Profile</a></li>
+                <li><span class="text-uppercase font-semi-bold">YouTube</span><br><a href="https://www.youtube.com/user/socratavideos" target="_blank" class="font-normal">Watch Videos</a></li>
+              </ul>
+            </div>
+          </div>         
+        </div>
+      </section>
+      <!-- Section 2 -->
+      <section id="section-2" class="section-padding background-light-grey-4">
+        <div class="row">
+          <div class="col-sm-8">
+            <p>Etiam porta sem malesuada magna mollis euismod. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>            
+          </div>
+          <div class="col-sm-4">
+            <p>Etiam porta sem malesuada magna mollis euismod. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>            
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </div>
 
-
-
-
-
-
-    </div>
-  </div>
-</section>
-<style>html[style] {margin-top:0 !important;}</style>
-<script>
-$(document).ready(function(){
-$('.responsive-tabs').responsiveTabs({
-  accordionOn: ['xs', 'sm'] // xs, sm, md, lg
-});
-});
+<script type="text/javascript">
+  $(function() {
+    var offset = $("#sidebar").offset();
+    var topPadding = 140;
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > offset.top) {
+        $("#sidebar").stop().animate({
+          marginTop: $(window).scrollTop() - offset.top + topPadding
+        });
+        } else {
+        $("#sidebar").stop().animate({
+          marginTop: 0
+        });
+      };
+    });
+  });
 </script>
